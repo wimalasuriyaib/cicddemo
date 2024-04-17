b0acb947335b05f2dcf5f1e457a03a95b50dfda6
Indika Wimalasuriya <49092020+wimalasuriyaib@users.noreply.github.com>
2024-04-17 07:37:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Indika Edit <code>src/App.js</code> and save to reload.
+          CICD Demo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
